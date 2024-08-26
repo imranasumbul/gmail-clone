0f9b2b14f30b9b9260a7d7743ada20ee9891fd7d
Imrana Sumbul <imranasumbul1211@gmail>
2024-08-26 17:29:40 +0530

    removed unnecessary import statements

--- client/src/components/sidebar/SentSidebar.tsx
+++ client/src/components/sidebar/SentSidebar.tsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useRecoilValue } from "recoil";
 import { userEmail } from "../../store/atoms/userEmail";
@@ -19,12 +19,12 @@ const SentSidebar = function() {
     };
 
     return (
-        <Link onClick={handleSentClick} className="flex justify-between w-[50%]" >
+        <a onClick={handleSentClick} className="flex justify-between w-[50%]" >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
             </svg>
             <span>Sent</span>
-        </Link>
+        </a>
     );
 };
 
