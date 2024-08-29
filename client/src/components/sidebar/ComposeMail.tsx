8fbd867679e1a1986eba436966c0179f4a041733
Imrana Sumbul <imranasumbul1211@gmail>
2024-08-29 16:35:40 +0530

    Added cursor pointer functionality to all sidebar items

--- client/src/components/sidebar/ComposeMail.tsx
+++ client/src/components/sidebar/ComposeMail.tsx
@@ -10,7 +10,7 @@ const ComposeMail = function(){
         
         <a onClick={function(){
             setComposeMail(true);
-        }} className="flex justify-center space-x-2 py-4 w-[70%] bg-blue-100 rounded-xl" > 
+        }} className="flex cursor-pointer justify-center space-x-2 py-4 w-[70%] bg-blue-100 rounded-xl" > 
             <svg xmlns="http://www.w3.org/2000/svg" stroke-width="2" fill="none" viewBox="0 0 24 24"  stroke="currentColor" className="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
             </svg>
--- client/src/components/sidebar/SnoozedSidebar.tsx
+++ client/src/components/sidebar/SnoozedSidebar.tsx
@@ -4,7 +4,7 @@ const SnoozedSidebar = function(){
     return (
         <>
        
-        <a className="flex justify-between w-[50%]" > 
+        <a className="flex cursor-pointer justify-between w-[50%]" > 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
